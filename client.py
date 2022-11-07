75a4e4af84e37675c39f5f547e6be2394a2b2a90
chaitanyagarg <chaitanya.gargsg@gmail.com>
2022-11-07 11:53:10 +0530

    Major Improvement. Basic Module to Work on.

--- client.py
+++ client.py
@@ -1,19 +1,53 @@
 import socket
-ClientMultiSocket = socket.socket()
+import select
+import sys
+
+connectionSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '127.0.0.1'
-port = 5000
+port = 5003
 print('Waiting for connection response')
 try:
-    ClientMultiSocket.connect((host, port))
+    connectionSocket.connect((host, port))
 except socket.error as e:
     print(str(e))
-res = ClientMultiSocket.recv(1024)
+while True:    
+    username = input("Enter Username: ")
+    connectionSocket.send(username.encode('utf-8'))
+    response = (connectionSocket.recv(1024)).decode('utf-8')
+    if response == "Connected":
+        print (f'Connection Established! Welcome Back, {username}')
+        break
+    else:
+        print ("Username is invalid, try again!")
+
+sockets = [connectionSocket, sys.stdin]
+
 while True:
-    Input = input('Hey there: ')
-    ClientMultiSocket.send(str.encode(Input))
-    res = ClientMultiSocket.recv(1024)
-    print(res.decode('utf-8'))
-ClientMultiSocket.close()
+    readSocket, x, errorSocket = select.select(sockets,[],sockets)
+    
+    for inputSocket in readSocket:
+        if inputSocket == connectionSocket:
+            msg = inputSocket.recv(1024)
+            print(msg.decode('utf-8'))
+
+            if not msg:
+                sockets = []
+                break
+                
+        else:
+            try:
+                msg = inputSocket.readline()
+                connectionSocket.send(msg.encode('utf-8'))
+            except:
+                pass
+            
+    if sockets == []:
+        break
+connectionSocket.close()
+
+
+        
+
 
 
 
